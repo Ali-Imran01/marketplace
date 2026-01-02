09008087490a837b0991cbba2e54d710a1d6a2ea
Ali-Imran01 <aliimranrohaizi.air@gmail.com>
2026-01-02 22:26:08 +0800

    feat: Initialize new Laravel project with React, TailwindCSS, and basic configuration files.

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\ItemController;
+use App\Http\Controllers\API\TransactionController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/test-db', function () {
+    try {
+        \Illuminate\Support\Facades\DB::connection()->getPdo();
+        return "Connected!";
+    } catch (\Exception $e) {
+        return "Failed: " . $e->getMessage();
+    }
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+Route::apiResource('items', ItemController::class);
+Route::apiResource('transactions', TransactionController::class)->middleware('auth:sanctum');
