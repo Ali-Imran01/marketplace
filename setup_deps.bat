90941f9d00fa577e6cf93520c8ec1578b467b196
Ali-Imran01 <aliimranrohaizi.air@gmail.com>
2026-01-02 22:26:37 +0800

    initial: Scaffold new Laravel project with basic configuration, dependencies, and testing setup.

---
+++ setup_deps.bat
@@ -0,0 +1,2 @@
+npm install "@vitejs/plugin-react" react react-dom react-router-dom axios tailwindcss postcss autoprefixer "@mui/material" "@emotion/react" "@emotion/styled" react-i18next i18next --save-dev
+npx tailwindcss init -p
